0d9c089364f8ea67c32a553599c4d3e58bb7ba64
Rafael Mendes <rafael.mendes@creditas.com>
2021-02-21 22:18:22 -0300

    server - get jobs for a company

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,11 +3,16 @@ const db = require("./db");
 const Query = {
   jobs: () => db.jobs.list(),
   job: (_root, args) => db.jobs.get(args.id),
-  company: (_root, args) => db.companies.get(args.id)
+  company: (_root, args) => db.companies.get(args.id),
 };
 
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
 
-module.exports = { Query, Job };
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
+module.exports = { Query, Job, Company };
