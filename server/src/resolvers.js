247521f340c9384f2c3c624795ee2f8545f842d8
Rafael Mendes <rafael.mendes@creditas.com>
2021-02-21 22:02:57 -0300

    server - get company by id

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,8 @@ const db = require("./db");
 
 const Query = {
   jobs: () => db.jobs.list(),
-  job: (_root, args) => db.jobs.get(args.id)
+  job: (_root, args) => db.jobs.get(args.id),
+  company: (_root, args) => db.companies.get(args.id)
 };
 
 const Job = {
