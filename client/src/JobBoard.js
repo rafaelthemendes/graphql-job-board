559d5dd48efaaf77e788ba4926c41913523dfea9
Rafael Mendes <rafael.mendes@creditas.com>
2021-02-21 21:00:43 -0300

    client - load jobs form the graphql endpoint

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import { JobList } from './JobList';
-const { jobs } = require('./fake-data');
+import React, { useEffect, useState } from "react";
+import { JobList } from "./JobList";
+import { loadJobs } from "./requests";
 
-export class JobBoard extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="title">Job Board</h1>
-        <JobList jobs={jobs} />
-      </div>
-    );
-  }
-}
+export const JobBoard = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const data = await loadJobs();
+      setJobs(data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">Job Board</h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+};
