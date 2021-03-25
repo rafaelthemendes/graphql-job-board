16b9353314fe3981de824248ea7af240001fbcf9
Rafael Mendes <rafael.mendes@creditas.com>
2021-03-25 14:29:35 -0300

    server - create job mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,6 +6,13 @@ const Query = {
   company: (_root, args) => db.companies.get(args.id),
 };
 
+const Mutation = {
+  createJob: (_root, {input}) => {
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  }
+}
+
 const Job = {
   company: (job) => db.companies.get(job.companyId),
 };
@@ -15,4 +22,4 @@ const Company = {
     db.jobs.list().filter((job) => job.companyId === company.id),
 };
 
-module.exports = { Query, Job, Company };
+module.exports = { Query, Mutation, Job, Company };
